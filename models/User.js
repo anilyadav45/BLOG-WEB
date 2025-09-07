3dd0d747ac74fcc7d58a838c4d52e567e7336bf0
Anil Yadav <rebelanil885@gmail.com>
2025-09-08 00:08:11 +0530

    module

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     username: String,
-    password: String
+    password: String,
+    imageUrl : {
+        type:String,
+        default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png"
+    }
 });
 
 const User = mongoose.model('User', userSchema);
